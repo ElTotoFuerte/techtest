1d8841ed9362174ff0b876fa8c8bc8ccdf2e064c
Thomas Gellier <thomas.gellier@gmail.com>
2022-05-05 18:25:49 -0400

    final iteration on the technical test, I didn't manage to display the modal if all required fields are filled nor did I manage to add the first name and email domain

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(function() {
+//   $("#submit-button").click(function() {
+//       if($("#name").val() !="" && $("#email").val() !="" && $("#checkbox").val() !="") {
+//         $('#myModal').modal('show');
+//         $(this).closest("form").submit();
+//       }
+//   });
+// });
